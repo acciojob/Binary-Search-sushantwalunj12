3d6d1d9a901b19830cb356179a945cc576537a9a
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-02-21 18:45:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ function binarySearch(nums, target) {
 // Your code here
 // Return index if target is present
 // else return -1
+
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+  let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
 }
 
 module.exports = binarySearch;
